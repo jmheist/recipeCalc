ecc098dc05d9c8ced81a68d8c15999c976bb602d
Jacob Heisterkamp <jheister@meredith.com>
2016-05-24 15:48:02 -0500

    Added Firebase

---
+++ Pods/Target Support Files/Pods-recipeCalc/Pods-recipeCalc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_recipeCalc : NSObject
+@end
+@implementation PodsDummy_Pods_recipeCalc
+@end
---
+++ Pods/Target Support Files/Pods-recipeCalcUITests/Pods-recipeCalcUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_recipeCalcUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_recipeCalcUITestsVersionString[];
+
