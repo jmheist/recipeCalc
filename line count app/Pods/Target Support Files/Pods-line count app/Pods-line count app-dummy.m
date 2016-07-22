24060b2a6b257a4fc7c73816b62ab84f9630dcd4
Jacob Heisterkamp <jheister@meredith.com>
2016-07-22 13:22:54 -0500

    pods

---
+++ line count app/Pods/Target Support Files/Pods-line count app/Pods-line count app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_line_count_app : NSObject
+@end
+@implementation PodsDummy_Pods_line_count_app
+@end
---
+++ line count app/Pods/Target Support Files/Pods-line count app/Pods-line count app-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_line_count_appVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_line_count_appVersionString[];
+
